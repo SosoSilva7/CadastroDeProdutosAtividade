3f9b0ec818adc091a1db4d2fc0639f49076d0065
Sofia <sfcrdsslv@gmail.com>
2025-04-28 23:00:39 -0300

    Parte Final do projeto PAM (excluir, foto, save)

--- CadastroDeProdutosAtividade/CadastroDeProdutosAtividade/ListaProdutosPage.xaml.cs
+++ CadastroDeProdutosAtividade/CadastroDeProdutosAtividade/ListaProdutosPage.xaml.cs
@@ -1,11 +1,12 @@
 namespace CadastroDeProdutosAtividade;
-
+using Microsoft.Maui.Storage;
+using System.Text.Json;
 public partial class ListaProdutosPage : ContentPage
 {
-	public ListaProdutosPage()
-	{
-		InitializeComponent();
-	}
+    public ListaProdutosPage()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnAppearing()
     {
@@ -16,7 +17,45 @@ public partial class ListaProdutosPage : ContentPage
         // p representa cada produto separadamnete, e cada um vai ser ordenado pela validade
         produtosListView.ItemsSource = MainPage.Produtos.OrderBy(p => p.Validade).ToList();
 
+        var produtos = produtosListView.ItemsSource as List<Produto>;
+
+        int quantidade = produtos?.Count ?? 0;
+
+        double total = produtos?.Sum(p => p.Preco) ?? 0;
+
+        resumoLabel.Text = $"Total: {quantidade} produto(s) - Valor: R$ {total:F2}";
+
+
+        produtosListView.ItemsSource = produtos;
+
+        //alertando os vencidos
+        var hoje = DateTime.Today;
+
+        var vencidos = produtos.Where(p => p.Validade.HasValue && p.Validade.Value < hoje).ToList();
+
+        var proximos = produtos.Where(p => p.Validade.HasValue && p.Validade.Value <= hoje.AddDays(3) && p.Validade.Value >= hoje).ToList();
+
+
+        if (vencidos.Any() || proximos.Any())
+
+        {
+
+            alertaLabel.Text = $" Atenção: {vencidos.Count} vencido(s), {proximos.Count} prestes a vencer!";
+
+        }
+
+        else
+
+        {
+
+            alertaLabel.Text = string.Empty;
+
+        }
+
     }
+
+
+
     private void FiltrarPorCategoria(object sender, EventArgs e)
     {
         string categoriaSelecionada = filtroCategoriaPicker.SelectedItem?.ToString() ?? "Todas";
@@ -49,5 +88,52 @@ public partial class ListaProdutosPage : ContentPage
     private async void Voltar_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MainPage());
+    }
+
+    private async void Remover_Clicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender; // o sender traz quem disparou o clique, no caso o btnremover
+        var produto = (Produto)button.BindingContext;
+
+        bool confirma = await DisplayAlert("Confirmação",
+    $"Deseja remover o produto \"{produto.Nome}\"?",
+    "Sim", "Não");
+
+        if (confirma && produto != null && MainPage.Produtos.Contains(produto))
+        {
+            MainPage.Produtos.Remove(produto);
+            ProdutoStorage.SalvarProdutos(MainPage.Produtos);
+
+        }
+        produtosListView.ItemsSource = null;
+        produtosListView.ItemsSource = MainPage.Produtos;
+        await Navigation.PushAsync(new ListaProdutosPage());
+        Navigation.RemovePage(this);
+    }
+
+    public static class ProdutoStorage
+    {
+        const string ProdutosKey = "ProdutosSalvos";
+
+        public static void SalvarProdutos(List<Produto> produtos)
+
+        {
+            string json = JsonSerializer.Serialize(produtos);
+
+            Preferences.Set(ProdutosKey, json);
+
+        }
+
+
+        public static List<Produto> CarregarProdutos()
+        {
+            string json = Preferences.Get(ProdutosKey, string.Empty);
+
+            return string.IsNullOrEmpty(json) ? new List<Produto>() :
+
+            JsonSerializer.Deserialize<List<Produto>>(json) ?? new List<Produto>();
+        }
+
+
     }
 }
--- CadastroDeProdutosAtividade/CadastroDeProdutosAtividade/MainPage.xaml.cs
+++ CadastroDeProdutosAtividade/CadastroDeProdutosAtividade/MainPage.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Maui.Controls;
+using static CadastroDeProdutosAtividade.ListaProdutosPage;
 
 namespace CadastroDeProdutosAtividade
 {
     public partial class MainPage : ContentPage
     {
-        public static List<Produto> Produtos { get; private set; } = new List<Produto>();
+        public static List<Produto> Produtos { get; set; } = ProdutoStorage.CarregarProdutos();
 
         private DateTime? DataValidadeSelecionada = null;
 
@@ -14,8 +15,6 @@ namespace CadastroDeProdutosAtividade
         {
             InitializeComponent();
         }
-
-
 
         private void ValidadePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
@@ -33,21 +32,28 @@ namespace CadastroDeProdutosAtividade
 
                 if (double.TryParse(precoEntry.Text, out double preco) && !string.IsNullOrWhiteSpace(nome))
                 {
+                    string caminhoImagem = string.IsNullOrEmpty(caminhoImagemSelecionada) ? "produtosemimagem.png" : caminhoImagemSelecionada;
                     Produtos.Add(new Produto
                     {
                         Nome = nome,
                         Preco = preco,
                         Descricao = descricao,
                         Validade = DataValidadeSelecionada,
-                        Categoria = categoria
-                    });
+                        Categoria = categoria,
+                        CaminhoImagem = caminhoImagemSelecionada
 
+                    });
+                    //produto.CaminhoImagem = caminhoImagemSelecionada;
+
+                    ProdutoStorage.SalvarProdutos(MainPage.Produtos);
                     mensagemLabel.Text = "Produto cadastrado com sucesso!";
 
                     // Limpa os campos
                     nomeEntry.Text = precoEntry.Text = descriEntry.Text = categoriaEntry.Text = string.Empty;
                     validadePicker.Date = DateTime.Today;
                     DataValidadeSelecionada = null;
+                    caminhoImagemSelecionada = null;
+                    previewImagem.Source = null;
                 }
                 else
                 {
@@ -64,7 +70,31 @@ namespace CadastroDeProdutosAtividade
         {
             await Navigation.PushAsync(new ListaProdutosPage());
         }
+
+        private string caminhoImagemSelecionada;
+        private async void SelecionarImagem_Clicked(object sender, EventArgs e)
+        {
+            var resultado = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Selecione uma imagem",
+
+                FileTypes = FilePickerFileType.Images
+
+            });
+
+
+            if (resultado != null)
+
+            {
+                caminhoImagemSelecionada = resultado.FullPath;
+
+                previewImagem.Source = ImageSource.FromFile(caminhoImagemSelecionada);
+
+            }
+
+        }
     }
+
     public class Produto
     {
         public string Nome { get; set; }
@@ -73,11 +103,19 @@ namespace CadastroDeProdutosAtividade
         public DateTime? Validade { get; set; }// DateTime? significa que pode ser nula
         public string Categoria { get; set; }
 
+        private string? _caminhoImagem;//guarda o valor real
+        public string CaminhoImagem
+        {
+            get => string.IsNullOrEmpty(_caminhoImagem) ? "produtosemimagem.png" : _caminhoImagem;
+            set => _caminhoImagem = value;
+        }
+
         public string PrecoFormatado => $"R$ {Preco:F2}";
 
         // se a validade for uma data válida, vai formatar...se nao ela será nula e retorna " "
         public string ValidadeFormatada => Validade?.ToString("dd/MM/yyyy") ?? "-";
         public bool EstaVencido => Validade.HasValue && Validade.Value.Date < DateTime.Today;
+
 
         public Produto(string nome, double preco, string descricao, DateTime? validade, string categoria)
         {
@@ -97,3 +135,4 @@ namespace CadastroDeProdutosAtividade
         }
     }
 }
+
